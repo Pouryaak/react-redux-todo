3f4d84f9b3ab92633f6c56e697d55c6100730bbf
Pourya Akrami <pourya@Pouryas-MacBook-Pro.local>
2021-06-02 15:57:24 +0800

    added empty avatar

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,10 +1,23 @@
 import React from "react";
 import Tasks from "./Tasks/Tasks";
+import { emptyAvatar } from "../components/UI/emptySVG";
+import { useSelector } from "react-redux";
+import { selectTasks } from "../feature/tasksSlice";
+import { Header } from "semantic-ui-react";
 
 export default function HomePage() {
+  const tasks = useSelector(selectTasks);
   return (
     <div>
       <Tasks />
+      {tasks.length === 0 && (
+        <>
+          <Header as="h2" icon textAlign="center">
+            Huurayy! No tasks
+          </Header>
+          {emptyAvatar}
+        </>
+      )}
     </div>
   );
 }
